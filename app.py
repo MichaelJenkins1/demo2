f7c2dd5f1eebe1604f202566e5a7e152de84d90a
Michael Jenkins <michael.jenkins@ludisanalytics.com>
2023-05-31 10:15:17 -0400

    app.py commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 airbnb_data = pd.read_csv("http://localhost:3005/v1/api/ludisurl/9e3a5f8")
 
 # View the first 5 rows
-airbnb_data.head()
+print(airbnb_data.head())
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', debug=True, port=8050)
